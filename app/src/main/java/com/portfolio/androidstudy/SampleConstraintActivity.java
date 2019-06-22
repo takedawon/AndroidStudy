8e81f8339e9aef08ebef8169c6b2c66e8ca420ed
takedawon <takedawon@naver.com>
2019-06-23 02:27:52 +0900

    2_1_3 레이아웃 기초 익히기

---
+++ app/src/main/java/com/portfolio/androidstudy/SampleConstraintActivity.java
@@ -0,0 +1,13 @@
+package com.portfolio.androidstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_constraint);
+    }
+}
