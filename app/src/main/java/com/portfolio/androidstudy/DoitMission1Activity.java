45deadc8c4badb52ee4359a5ac56f36b5e9eddf2
takedawon <takedawon@naver.com>
2019-06-23 02:30:50 +0900

    도전 01 화면 아래쪽에 두 개의 버튼 추가하기

---
+++ app/src/main/java/com/portfolio/androidstudy/DoitMission1Activity.java
@@ -0,0 +1,13 @@
+package com.portfolio.androidstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoitMission1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doit_mission1);
+    }
+}
