bf6713bf7b012c3e91438a5a1b77144c5d80005d
takedawon <takedawon@naver.com>
2019-06-27 12:45:44 +0900

    2-4 테이블 레이아웃

---
+++ app/src/main/java/com/portfolio/androidstudy/session2/SampleTableActivity.java
@@ -0,0 +1,15 @@
+package com.portfolio.androidstudy.session2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.portfolio.androidstudy.R;
+
+public class SampleTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_table);
+    }
+}
