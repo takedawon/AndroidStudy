a19d63d9bce693ec587d68f84a4eb1318ab6982f
takedawon <takedawon@naver.com>
2019-07-01 15:34:09 +0900

    3-1 기본 위젯 다시 한 번 자세히 공부하기

---
+++ app/src/main/java/com/portfolio/androidstudy/session3/SampleWidgetActivity.java
@@ -0,0 +1,15 @@
+package com.portfolio.androidstudy.session3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.portfolio.androidstudy.R;
+
+public class SampleWidgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button);
+    }
+}
