b4594b94a1012157e4c67f32bed3579ff0a56082
takedawon <takedawon@naver.com>
2019-07-12 10:22:35 +0900

    4-1 레이아웃 인플레이션 이해하기

---
+++ app/src/main/java/com/portfolio/androidstudy/session3/SampleLayoutInflaterActivity.java
@@ -0,0 +1,37 @@
+package com.portfolio.androidstudy.session3;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.portfolio.androidstudy.R;
+
+public class SampleLayoutInflaterActivity extends AppCompatActivity {
+    LinearLayout container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_layout_inflater);
+        container=findViewById(R.id.container2);
+
+        Button button=findViewById(R.id.button40);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater=(LayoutInflater)
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1,container,true);
+                CheckBox checkBox = container.findViewById(R.id.checkBox2);
+                checkBox.setText("로딩되었어요");
+            }
+        });
+    }
+}
